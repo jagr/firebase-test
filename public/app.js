a6a8b1419fdbcd5cbadce1ae5893a99eefba4c72
Jack Griffiths <Saturnus@Saturnus.local>
2020-10-24 17:15:10 -0700

    connecting to firestore

--- public/app.js
+++ public/app.js
@@ -1,5 +1,4 @@
 const auth = firebase.auth(); //auth sdk
-
 const whenSignedIn = document.getElementById('whenSignedIn');
 const whenSignedOut = document.getElementById('whenSignedOut');
 
@@ -12,5 +11,37 @@ const provider = new firebase.auth.GoogleAuthProvider();
 
 signInBtn.onclick = () => auth.signInWithPopup(provider);
 signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3><p>User ID: ${user.uid}</p>`
+    } else {
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
+
+const db = firebase.firestore();
+const createThing = document.getElementById('createThing');
+const thingsList = document.getElementById('thingsList');
+
+let thingsRef;
+let unsubscribe;
+
+auth.onAuthStateChanged(user => {
+    if(user) {
+        thingsRef = db.collection('things');
+        createThing.onclick = () => {
+            thingsRef.add({
+                uid: user.uid,
+                name: faker.commerce.productName(),
+                createdAt: serverTimestamp()
+            });
+        }
+    }
+})
 
 console.log(firebase)
